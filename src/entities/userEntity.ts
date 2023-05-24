ac1bb644dad2f00834011d834cc12488d58da145
lucana-m <lucana_monica@hotmail.com>
2023-05-23 22:33:37 -0300

    feat: create User and Contact Entities

---
+++ src/entities/userEntity.ts
@@ -0,0 +1,45 @@
+import { getRounds, hashSync } from "bcryptjs";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BeforeInsert,
+  BeforeUpdate,
+  CreateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Contact } from "./contactsEntity";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 60, unique: true })
+  email: string;
+
+  @Column({ type: "integer" })
+  phone: number;
+
+  @Column({ length: 120 })
+  password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPasssword() {
+    const isEncripted = getRounds(this.password);
+
+    if (!isEncripted) {
+      this.password = hashSync(this.password, 10);
+    }
+  }
+
+  @OneToMany(() => Contact, (contact) => contact.user)
+  contacts: Contact[];
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: string | Date;
+}
